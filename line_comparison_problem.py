ebd58778f2a278d564f34e7fd419eccc1fc59936
pavank3673 <pavank3673@outlook.com>
2024-12-27 11:03:53 +0530

    [Pavan Kumar]Add:check equality of two lines

--- line_comparison_problem.py
+++ line_comparison_problem.py
@@ -19,7 +19,11 @@ class Line:
     def calculate_length(self):
         return sqrt(((self.point_two.x - self.point_one.x) ** 2) + ((self.point_two.y - self.point_one.y) ** 2))
 
-
+    def __eq__(self, value):
+        if isinstance(value, self.__class__):
+            return self.calculate_length() == value.calculate_length()
+        else:
+            return False
 
 sample_line = Line(Point(1, 1), Point(3, 3))
 
@@ -28,3 +32,11 @@ print(sample_line)
 sample_line_length = sample_line.calculate_length()
 
 print(f"The length of line with co-ordinates ({sample_line.point_one.x}, {sample_line.point_one.y}) & ({sample_line.point_two.x}, {sample_line.point_two.y}) = {sample_line_length}")
+
+sample_line_two = Line(Point(1, 1), Point(3, 3))
+line_equals = sample_line == sample_line_two
+
+if line_equals:
+    print(f"The two lines with co-ordinates of Line 1, Point 1({sample_line.point_one.x}, {sample_line.point_one.y}) & Point 2({sample_line.point_two.x}, {sample_line.point_two.y}) and Line 2 Point 1({sample_line_two.point_one.x}, {sample_line_two.point_one.y}) & Point 2({sample_line_two.point_two.x}, {sample_line_two.point_two.y}) are equal")
+else:
+    print(f"The two lines with co-ordinates of Line 1, Point 1({sample_line.point_one.x}, {sample_line.point_one.y}) & Point 2({sample_line.point_two.x}, {sample_line.point_two.y}) and Line 2 Point 1({sample_line_two.point_one.x}, {sample_line_two.point_one.y}) & Point 2({sample_line_two.point_two.x}, {sample_line_two.point_two.y}) are not equal")
